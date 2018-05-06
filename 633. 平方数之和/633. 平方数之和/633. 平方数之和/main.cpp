0df2a26f71f9706e334a979ed35509781ac16ca3
huwen <huwen1004>
2018-05-06 19:12:37 +0800

    633

---
+++ 633. 平方数之和/633. 平方数之和/633. 平方数之和/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  633. 平方数之和
+//
+//  Created by clown on 2018/5/6.
+//  Copyright © 2018 clown. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
