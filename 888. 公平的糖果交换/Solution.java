171bdc587dc940659d0e993751ddc8ec64796de3
huwen <huwen1004@gmail.com>
2018-09-11 22:18:40 +0800

    888

---
+++ 888. 公平的糖果交换/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] fairCandySwap(int[] A, int[] B) {
+        int sumA = 0;
+        int sumB = 0;
+        for (int num: A) {
+            sumA += num;
+        }
+        for (int num: B) {
+            sumB += num;
+        }
+        
+        int delta = (sumA - sumB) / 2;
+        Set<Integer> set = new HashSet();
+        for (int num: A) {
+            set.add(num);
+        }
+        
+        for (int num: B) {
+            if (set.contains(num + delta)) {
+                return new int[]{num + delta, num};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
